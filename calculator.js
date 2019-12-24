e3a7dc446d3540cd751f8cf3d4c32407046a5ede
Ryczko <Ryczko@users.noreply.github.com>
2019-12-24 13:52:27 +0100

    poprawione

--- calculator.js
+++ calculator.js
@@ -1,35 +1,27 @@
-let startVal = 0;
-let secondVal = 0;
+let startVal = "0";
+let secondVal = "0";
 let symbol = null;
 let comma = false;
 let isChoosen = false;
 
-const numbers = document.querySelectorAll('.numbers');
 const result = document.querySelector('.result');
-const symbols = document.querySelectorAll('.symbol');
-const c = document.querySelector('.c');
-const ce = document.querySelector('.ce');
+const container = document.querySelector('.container');
 
-symbols.forEach(el => {
-    el.addEventListener('click', getSymbol);
-})
-
-numbers.forEach(el => {
-    el.addEventListener('click', write);
-})
-
-c.addEventListener('click', clearAll);
-ce.addEventListener('click', clearNumber);
-
+container.onclick = function (event) {
+    const classes = event.target.classList;
+    if (classes.contains("numbers")) write(event);
+    else if (classes.contains("symbol")) getSymbol(event);
+    else if (classes.contains("c")) clearAll();
+    else if (classes.contains("ce")) clearNumber();
+}
 
 function clearNumber() {
-    startVal = 0;
-    result.value = startVal;
+    startVal = "0";
 }
 
 function clearAll() {
-    startVal = 0;
-    secondVal = 0;
+    startVal = "0";
+    secondVal = "0";
     symbol = null;
     comma = false;
     isChoosen = false;
@@ -39,17 +31,11 @@ function clearAll() {
 function write(e) {
     const val = e.target.textContent;
 
-    if (startVal == "0" && val != ",") {
-        startVal = val;
-    }
-    else if (val != ",") {
-        startVal += val;
-    }
-    else {
-        if (comma == false) {
-            startVal += ".";
-            comma = true;
-        }
+    if (startVal === "0" && val !== ",") startVal = val;
+    else if (val !== ",") startVal += val;
+    else if (comma === false) {
+        startVal += ".";
+        comma = true;
     }
     result.value = startVal;
     isChoosen = true;
@@ -57,23 +43,22 @@ function write(e) {
 
 function getSymbol(e) {
     if (isChoosen) {
-        if (symbol != "=" && symbol != null) count(startVal, symbol, secondVal);
+        if (symbol !== "=" && symbol !== null) count(startVal, symbol, secondVal);
         secondVal = startVal;
-        startVal = 0;
+        startVal = "0";
     };
     symbol = e.target.textContent;
     isChoosen = false;
     comma = false;
-
 }
 
 function count(sVal, symb, secVal) {
-
-    if (symb == "/" && sVal == "0") result.value = "Nie dziel przez 0";
-    else {
-        startVal = round(eval(secVal + symb + sVal), 14);
-        result.value = startVal;
+    startVal = round(eval(secVal + symb + sVal), 14);
+    if (symb == "/" && (startVal === Infinity || startVal === -Infinity)) {
+        clearAll();
+        result.value = "Nie dziel przez 0";
     }
+    else result.value = startVal;
 }
 
 //Pominięcie błędów podczas operacji na float
